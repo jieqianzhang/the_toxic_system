1d3d5989cf114c3b98d1a9c2cada06350310ed21
jieqianzhang <zhang.jieqian@gmail.com>
2016-04-28 13:32:04 -0700

    update

--- js/main.js
+++ js/main.js
@@ -4,3 +4,35 @@
 //        scrollTop: $("#intro_page").offset().top
 //    }, 600);
 //});
+var isChrome = !!window.chrome; 
+var isIE = /*@cc_on!@*/false;
+
+if( isChrome ) {
+$("#videoId").replaceWith($('<video id="videoId" width="100%" autoplay loop><source src="video.webm" type="video/webm"></video>'));
+}
+
+$("#menu_home").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#landing_page").offset().top
+    }, 600);
+});
+
+$("#menu_intro").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#intro_page").offset().top
+    }, 600);
+});
+
+
+$("#osvaldo").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#section1_div").offset().top
+    }, 600);
+});
+
+$("#menu_section1").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#section1_div").offset().top
+    }, 600);
+});
+
