27b275e38a9cf9ca3ccd7edf548c0a0bfa1e5232
jieqianzhang <zhang.jieqian@gmail.com>
2016-04-28 22:11:23 -0700

    video_embed

--- js/main.js
+++ js/main.js
@@ -1,38 +1,62 @@
-
 //$("#landing_page").on('mousewheel', function() {
 //    $('html, body').animate({
 //        scrollTop: $("#intro_page").offset().top
 //    }, 600);
 //});
-var isChrome = !!window.chrome; 
-var isIE = /*@cc_on!@*/false;
+var isChrome = !!window.chrome;
+var isIE = /*@cc_on!@*/ false;
 
-if( isChrome ) {
-$("#videoId").replaceWith($('<video id="videoId" width="100%" autoplay loop><source src="video.webm" type="video/webm"></video>'));
+if (isChrome) {
+    $("#videoId").replaceWith($('<video id="videoId" width="100%" autoplay loop><source src="video.webm" type="video/webm"></video>'));
 }
 
-$("#menu_home").click(function() {
+$("#menu_home").click(function () {
     $('html, body').animate({
         scrollTop: $("#landing_page").offset().top
     }, 600);
 });
 
-$("#menu_intro").click(function() {
+$("#menu_intro").click(function () {
     $('html, body').animate({
         scrollTop: $("#intro_page").offset().top
     }, 600);
 });
 
-
-$("#osvaldo").click(function() {
+$("#menu_section1").click(function () {
     $('html, body').animate({
         scrollTop: $("#section1_div").offset().top
     }, 600);
 });
 
-$("#menu_section1").click(function() {
+
+$("#menu_section2").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#section2_div").offset().top
+    }, 600);
+});
+
+$("#menu_section3").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#section3_div").offset().top
+    }, 600);
+});
+
+$("#menu_section4").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#section4_div").offset().top
+    }, 600);
+});
+
+
+$("#menu_section5").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#section5_div").offset().top
+    }, 600);
+});
+
+
+$("#osvaldo").click(function () {
     $('html, body').animate({
         scrollTop: $("#section1_div").offset().top
     }, 600);
 });
-
